d793d7eda970c3325865403cc8fa683db0d1295f
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-04-01 17:30:51 -0400

    made nav bar with links that send user to different views, created js that allows user to create a post

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,6 +3,7 @@ const { User } = require('../../models');
 
 router.post('/login', async (req, res) => {
   try {
+    // Find the user who matches the posted e-mail address
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
@@ -12,6 +13,7 @@ router.post('/login', async (req, res) => {
       return;
     }
 
+    // Verify the posted password with the password store in the database
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
@@ -21,6 +23,7 @@ router.post('/login', async (req, res) => {
       return;
     }
 
+    // Create session variables based on the logged in user
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -35,6 +38,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
+    // Remove the session variables
     req.session.destroy(() => {
       res.status(204).end();
     });
---
+++ public/js/post.js
@@ -0,0 +1,75 @@
+const USERID = {
+  name: null,
+  identity: null,
+  image: null,
+  message: null,
+  date: null
+}
+
+const discussionName = document.querySelector(".dynamicId");
+const paragraph = document.querySelector("#discussionName");
+const userComment = document.querySelector(".usercomment");
+const publishBtn = document.querySelector("#publish");
+const comments = document.querySelector(".comments");
+const userName = document.querySelector(".user");
+const container = document.getElementById('container');
+
+userComment.addEventListener("input", () => {
+  if (!userComment.value) {
+    publishBtn.disabled = true;
+  } else {
+    publishBtn.disabled = false;
+  }
+});
+
+function addPost() {
+  if (!userComment.value) return;
+  USERID.name = userName.value;
+  if (!USERID.name) return;
+  else {
+    USERID.identity = true;
+  }
+
+  USERID.message = userComment.value;
+  USERID.date = new Date().toLocaleString();
+  let published = 
+  `<div id="parents">
+      <img src="${USERID.image}">
+      <div id="comment1">
+          <h1>${USERID.name}</h1>
+          <p>${USERID.message}</p>
+          <br>
+          <button onclick="deleteDiv1()">Delete</button>
+          <br>
+          <span class="date">${USERID.date}</span>
+      </div>    
+  </div>`
+
+  container.innerHTML += published;
+  userComment.value = "";
+  publishBtn.disabled = true;
+
+  updateCommentCount();
+}
+
+function updateCommentCount() {
+  let commentsNum = document.querySelectorAll(".date").length;
+  document.getElementById("comment").textContent = commentsNum;
+}
+
+function deleteDiv1() {
+  let element = document.getElementById("parents");
+  if (element) {
+    element.remove();
+    updateCommentCount();
+  }
+}
+
+publishBtn.addEventListener("click", addPost);
+
+if (container) {
+  container.innerHTML += published;
+  // Other actions
+} else {
+  console.error('Container element not found');
+}
